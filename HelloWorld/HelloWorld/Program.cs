8de06e32afcc49b45db99ffeb39c055a77d7e0a5
Erik Bergland <54935266+Bergan27@users.noreply.github.com>
2020-08-27 12:00:06 +0200

    Variabler och Datatyper

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -6,7 +6,39 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Niklas!");
+            Console.WriteLine("Skriv in namn");
+            string användarNamn = Console.ReadLine();
+            Console.WriteLine("Skriv in din ålder");
+            int ålder = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Lever du? 1=Ja/2=Nej");
+
+            bool livsstatus = true;
+            var ingenpuls = Console.ReadLine();
+
+            if ( ingenpuls == "1"){
+                livsstatus = true;
+
+            }
+            else
+            {
+                livsstatus = false;
+            }
+            if(livsstatus == true)
+            {
+                Console.WriteLine("Ditt namn är " + användarNamn);
+                Console.WriteLine("Din ålder är " + ålder);
+                Console.WriteLine("Du lever fortfarande, fortsätt så");
+            }
+            else
+            {
+                Console.WriteLine("Ditt namn är" + användarNamn);
+                Console.WriteLine("Din ålder är" + ålder);
+                Console.WriteLine("Ajdå, RIP");
+            }
+                
+            
+            
+
         }
     }
 }
