9814f8e6854550547d41d724a270c229ce8368e3
Erik Bergland <54935266+Bergan27@users.noreply.github.com>
2020-10-13 12:36:09 +0200

    Lasse på heden

--- Metoder/Products.cs
+++ Metoder/Products.cs
@@ -4,10 +4,28 @@ using System.Text;
 
 namespace Metoder
 {
-    class Products
+    class Food
     {
         public string item1 = "Half special";
         public string item2 = "Hotdog";
-        public string item3 = "Big Strong";
+        public string item3 = "Hamburger";
+    }
+
+    class Tobacco 
+    {
+        public string item4 = "Ettan portion";
+        public string item5 = "General original";
+        public string item6 = "Camel breeze";
+        public string item7 = "Marlboro gold";
+    }
+    class Beer
+    {
+        public string item8 = "Carlsberg hof";
+        public string item9 = "Tuborg gold";
+        public string item10 = "Breznak";
+        public string item11 = "Franzikaner";
+        public string item12 = "Weihenstephaner";
+        public string item13 = "Guinness";
+
     }
 }
--- Metoder/Program.cs
+++ Metoder/Program.cs
@@ -8,11 +8,27 @@ namespace Metoder
         {
             Console.WriteLine("Hello and welcome to Lasse på Heden!");
             Customer customer = new Customer();
-            Products products = new Products();
+            Food food = new Food();
+            Tobacco tobacco = new Tobacco();
+            Beer beer = new Beer();
             Console.WriteLine("Start by looking trough our meny " +
+                "\n Food-----------------------" +
                 "\n item1 = Half special" +
                 "\n item2 = Hotdog" +
-                "\n item3 = Big Strong" +
+                "\n item3 = Hamburger" +
+                "\n Tobacco--------------------" +
+                "\n item4 = Ettan portion" +
+                "\n item5 = General original" +
+                "\n item6 = Camel breeze" +
+                "\n item7 = Marlboro gold" +
+                "\n Beer-----------------------" +
+                "\n item8 = Carlsberg hof" +
+                "\n item9 = Tuborg gold" +
+                "\n item10 = Breznak" +
+                "\n item11 = Franzikaner" +
+                "\n item12 = Weihenstephaner" +
+                "\n item13 = Guinness" +
+                "\n ---------------------------" +
                 "\n exit - Done ordering");
             string answer;
             int loopconditionNumber = 0;
@@ -25,16 +41,57 @@ namespace Metoder
                 {
                     case "item1":
                         Console.WriteLine("Item Half Special was added to your order");
-                        customer.shoppingCart.Add(products.item1);
+                        customer.shoppingCart.Add(food.item1);
                         break;
                     case "item2":
                         Console.WriteLine("Item Hotdog was added to your order");
-                        customer.shoppingCart.Add(products.item2);
+                        customer.shoppingCart.Add(food.item2);
                         break;
                     case "item3":
-                        Console.WriteLine("Item Big Strong was added to your order");
-                        customer.shoppingCart.Add(products.item3);
+                        Console.WriteLine("Item Hamburger was added to your order");
+                        customer.shoppingCart.Add(food.item3);
                         break;
+                    case "item4":
+                        Console.WriteLine("Item Ettan portion was added to your order");
+                        customer.shoppingCart.Add(tobacco.item4);
+                        break;
+                    case "item5":
+                        Console.WriteLine("Item General original was added to your order");
+                        customer.shoppingCart.Add(tobacco.item5);
+                        break;
+                    case "item6":
+                        Console.WriteLine("Item Camel breeze was added to your order");
+                        customer.shoppingCart.Add(tobacco.item6);
+                        break;
+                    case "item7":
+                        Console.WriteLine("Item Marlboro gold was added to your order");
+                        customer.shoppingCart.Add(tobacco.item7);
+                        break;
+                    case "item8":
+                        Console.WriteLine("Item Carlsberg hof was added to your order");
+                        customer.shoppingCart.Add(beer.item8);
+                        break;
+                    case "item9":
+                        Console.WriteLine("Item Tuborg gold was added to your order");
+                        customer.shoppingCart.Add(beer.item9);
+                        break;
+                    case "item10":
+                        Console.WriteLine("Item Breznak was added to your order");
+                        customer.shoppingCart.Add(beer.item10);
+                        break;
+                    case "item11":
+                        Console.WriteLine("Item Franzikaner was added to your order");
+                        customer.shoppingCart.Add(beer.item11);
+                        break;
+                    case "item12":
+                        Console.WriteLine("Item Weihenstephaner was added to your order");
+                        customer.shoppingCart.Add(beer.item12);
+                        break;
+                    case "item13":
+                        Console.WriteLine("Item Guinness was added to your order");
+                        customer.shoppingCart.Add(beer.item13);
+                        break;
+
                     case "exit":
                         loopconditionNumber = 1;
                         break;
